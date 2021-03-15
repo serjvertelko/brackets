539a2b5f60c9d18521a4fb4600412e95aaaf227f
Sergei <serjvertelko@gmail.com>
2021-03-15 23:42:14 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var stack = [];
+  var existBracket = {};
+  for(var j = 0; j < bracketsConfig.length; j += 1) {
+    existBracket[bracketsConfig[j][0]] = bracketsConfig[j][1];
+  }
+
+  stack.push(str.charAt(0));
+
+  for(var i = 1; i < str.length; i += 1) {
+    if(str.charAt(i) === existBracket[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(str.charAt(i)); 
+    }
+  }
+  if (stack.length > 0) {return false;}
+  return true;
+};
